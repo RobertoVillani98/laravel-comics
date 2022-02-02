3780730ce8500644ac5f59db2c3f09e852cac76b
RobertoVillani <robertovillani58@gmail.com>
2022-02-02 17:09:31 +0100

    header e jumbo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,10 @@
 
 @section('pageContent')
 
+<div class="jumbo">
+    <img src="{{asset('/img/jumbotron.jpg')}}" alt="jumbo image">
+</div>
+
 <div class="series">
     <div class="container">
         <h2>Current series</h2>
@@ -17,7 +21,58 @@
                 </a>
             @endforeach
         </div>
+
+<div class="row">
+    <a href="#" class="load-more">load more</a>
+</div>
+
+<div class="shop">
+    <div class="container">
+        <div class="row">
+
+            <div class="box">
+                <a href="#">
+                    <img src="{{asset('img\buy-comics-digital-comics.png')}}" alt="Digital comics">
+                    <span>digital comics</span>
+                </a>
+            </div>
+            
+            <div class="box">
+                <a href="#">
+                    <img src="{{asset('img\buy-comics-merchandise.png')}}" alt="DC merchandise">
+                    <span>dc merchandise</span>
+                </a>
+            </div>
+            
+            <div class="box">
+                <a href="#">
+                    <img src="{{asset('img\buy-comics-subscriptions.png')}}" alt="Subscription">
+                    <span>subscription</span>
+                </a>
+            </div>
+            
+            <div class="box">
+                <a href="#">
+                    <img src="{{asset('img\buy-comics-shop-locator.png')}}" alt="Comic shop locator">
+                    <span>comic shop locator</span>
+                </a>
+            </div>
+            
+            <div class="box">
+                <a href="#">
+                    <img src="{{asset('img\buy-dc-power-visa.svg')}}" alt="DC power VISA">
+                    <span>dc power visa</span>
+                </a>
+            </div>
+        
+        </div>
     </div>
 </div>
+
+
+
+
+
+    
 
 @endsection
