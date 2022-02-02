84783df615abb031d22db7c19afc063214aae8e9
RobertoVillani <robertovillani58@gmail.com>
2022-02-02 17:48:30 +0100

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,84 @@
-<footer></footer>
+<footer>
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="left">
+
+                    <div class="col">
+                        <h4>dc comics</h4>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movie</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+
+                        <h4>shop</h4>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+
+                    </div>
+
+                    <div class="col">
+                        <h4>dc</h4>
+                        <ul>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Provacy policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col">
+                        <h4>sites</h4>
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div class="right">
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+
+                <a href="#" class="sign-up">sing-up now!</a>
+
+                <ul>
+                    <li><a href="#"><span>follow us</span></a></li>
+                    <li><a href="#"><img src="{{asset('img\footer-facebook.png')}}" alt="facebook"></a></li>
+                    <li><a href="#"><img src="{{asset('img\footer-twitter.png')}}" alt="twitter"></a></li>
+                    <li><a href="#"><img src="{{asset('img\footer-youtube.png')}}" alt="youtube"></a></li>
+                    <li><a href="#"><img src="{{asset('img\footer-pinterest.png')}}" alt="pinterest"></a></li>
+                    <li><a href="#"><img src="{{asset('img\footer-periscope.png')}}" alt="periscope"></a></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+
+</footer>
