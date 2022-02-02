d37bd1f87d01386161353cd426d021e6440361c1
RobertoVillani <robertovillani58@gmail.com>
2022-02-02 16:35:20 +0100

    stampa card main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,4 +5,19 @@
 @endsection
 
 @section('pageContent')
+
+<div class="series">
+    <div class="container">
+        <h2>Current series</h2>
+        <div class="row">
+            @foreach ($comics as $comic)
+                <a href="#" class="card">
+                    <img src="{{$comic["thumb"]}}" alt="{{$comic["title"]}}">
+                    <span>{{$comic["title"]}}</span>
+                </a>
+            @endforeach
+        </div>
+    </div>
+</div>
+
 @endsection
